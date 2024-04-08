f252fc798212b759cae1c0859c13124ff74c0db8
opielapatryk <xpatrykopiela@gmail.com>
2024-04-08 11:49:50 +0200

    Dish entity tested

---
+++ menu/tests/domain/test_dish_entity.py
@@ -0,0 +1,9 @@
+from domain.dish import Dish
+
+def test_dish():
+    dish = Dish(1, "pizza", "pepperoni", 12.99)
+    assert dish.id == 1
+    assert dish.name == 'pizza'
+    assert dish.description == 'pepperoni'
+    assert dish.price == 12.99
+    
