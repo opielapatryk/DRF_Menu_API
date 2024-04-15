d4adebff781f2e6c9f9522d9281c124589cbba9d
opielapatryk <xpatrykopiela@gmail.com>
2024-04-15 09:42:35 +0200

    PATCH memrepo

--- menu/application/views.py
+++ menu/application/views.py
@@ -8,6 +8,7 @@ from domain.use_cases.dish_list_use_case import dish_list_use_case
 from domain.use_cases.dish_post_use_case import dish_post_use_case
 from domain.use_cases.dish_get_use_case import dish_get_use_case
 from domain.use_cases.dish_put_use_case import dish_put_use_case
+from domain.use_cases.dish_patch_use_case import dish_patch_use_case
 from domain.use_cases.dish_del_use_case import dish_delete_use_case
 from rest_framework import status
 
@@ -54,16 +55,16 @@ postgres_configuration = {
     "APPLICATION_DB": 'restaurant',
 }
 
+class Root(APIView):
+    def get(self,request):
+        return Response({'dishes': 'http://localhost:8000/api/v1/dishes/'})
+
 class DishView(APIView):
     def get(self, request, pk=None):
         if pk is not None:
             dish_id = pk
-            repo = MongoRepo(mongo_configuration)
+            repo = MemRepo(dishes)
             dish = dish_get_use_case(repo, dish_id)
-            print("+++++++++++++++++++++++++++++++++++++++++++++")
-            print(dish_id)
-            print(dish)
-            print("+++++++++++++++++++++++++++++++++++++++++++++")
             
             if dish:
                 serializer = DishSerializer(dish).data
@@ -71,7 +72,7 @@ class DishView(APIView):
             else:
                 return Response({"message": "Dish not found"}, status=status.HTTP_404_NOT_FOUND)
 
-        repo = MongoRepo(mongo_configuration)
+        repo = MemRepo(dishes)
         results = dish_list_use_case(repo)
         serializer = DishSerializer(results, many=True).data
         return Response(serializer)
@@ -79,33 +80,55 @@ class DishView(APIView):
     def post(self, request):
         serializer = DishSerializer(data=request.data)
         if serializer.is_valid():
-            repo = MongoRepo(mongo_configuration)
+            repo = MemRepo(dishes)
             new_dish_data = serializer.validated_data
             new_dish = dish_post_use_case(repo, new_dish_data)
-            serializer = DishSerializer(new_dish,many=True).data
-            return Response(serializer, status=status.HTTP_201_CREATED)
+            if new_dish:
+                serializer = DishSerializer(new_dish,many=True).data
+                return Response(serializer, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"message": "Dish id duplicated"}, status=status.HTTP_400_BAD_REQUEST)
+
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+    def patch(self, request, pk=None):
+        if pk is not None:
+            dish_id = pk
+            serializer = DishSerializer(data=request.data, partial=True)
+            if serializer.is_valid():
+                repo = MemRepo(dishes)
+                updated_dish_list = dish_patch_use_case(repo, serializer.validated_data,dish_id)
+                serializer = DishSerializer(updated_dish_list, many=True).data
+                if serializer:
+                    return Response(serializer, status=status.HTTP_200_OK)
+                else:
+                    return Response({"message": "Bad request"}, status=status.HTTP_400_BAD_REQUEST)
+            else:
+                return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)        
 
     def put(self, request):
         serializer = DishSerializer(data=request.data)
         if serializer.is_valid():
-            repo = MongoRepo(mongo_configuration)
+            repo = MemRepo(dishes)
             updated_dish_list = dish_put_use_case(repo, serializer.validated_data)
             serializer = DishSerializer(updated_dish_list, many=True).data
-            return Response(serializer, status=status.HTTP_201_CREATED)
+            if serializer:
+                return Response(serializer, status=status.HTTP_201_CREATED)
+            else:
+                return Response({"message": "Bad request"}, status=status.HTTP_400_BAD_REQUEST)
         else:
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def delete(self, request, pk=None):
         if pk is not None:
             dish_id = pk
-            repo = MongoRepo(mongo_configuration)
+            repo = MemRepo(dishes)
             dish = dish_delete_use_case(repo, dish_id)
             
             if dish:
                 serializer = DishSerializer(dish, many=True).data
-                return Response(serializer, status=status.HTTP_204_NO_CONTENT)
+                return Response(serializer, status=status.HTTP_200_OK)
             else:
                 return Response({"message": "Dish not found"}, status=status.HTTP_404_NOT_FOUND)
             
--- menu/infrastructure/repositories/memrepo.py
+++ menu/infrastructure/repositories/memrepo.py
@@ -10,33 +10,43 @@ class MemRepo:
     
    def get(self, id):
       try:
-         dish = [Dish.from_dict(dish) for dish in self.data if dish['id'] == id]
-         return dish
+         for dish in self.data:
+            if dish['id'] == id:
+               dish = Dish.from_dict(dish)
+               return dish 
+            
       except (IndexError, ValueError):
          return None
 
    def post(self, dish):
-      self.data.append(dish)
-      result = [Dish.from_dict(i) for i in self.data]
-
-      return result
+      if dish['id'] > len(self.data):
+         self.data.append(dish)
+         result = [Dish.from_dict(i) for i in self.data]
+         return result
 
    def put(self, updated_dish):
       for dish in self.data:
          if dish['id'] == updated_dish['id']:
-               dish['name'] = updated_dish['name']
-               dish['description'] = updated_dish['description']
-               dish['price'] = updated_dish['price']
+            dish['name'] = updated_dish['name']
+            dish['description'] = updated_dish['description']
+            dish['price'] = updated_dish['price']
 
-      result = [Dish.from_dict(i) for i in self.data]
+            result = [Dish.from_dict(i) for i in self.data]
+            return result
 
-      return result
+   def patch(self, updated_dish,dish_id):
+      for dish in self.data:
+         if dish['id'] == dish_id:
+               for key in updated_dish.keys():
+                  dish[key] = updated_dish[key]
+
+               result = [Dish.from_dict(i) for i in self.data]
+               return result
+         
    
    def delete(self, id):
       for dish in self.data:
          if dish['id'] == id:
                self.data.remove(dish)
-
-      result = [Dish.from_dict(dish) for dish in self.data]
-      
-      return result
+               result = [Dish.from_dict(dish) for dish in self.data]
+               return result
